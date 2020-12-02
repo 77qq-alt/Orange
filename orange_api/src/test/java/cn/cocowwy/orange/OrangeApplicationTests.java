1f93dfb6007395426c298d98ba4673d26e5c292e
wangwy8 <wangwy8@asiainfo.com>
2020-12-02 11:53:22 +0800

    初始化项目orange_api

---
+++ orange_api/src/test/java/cn/cocowwy/orange/OrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.orange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
