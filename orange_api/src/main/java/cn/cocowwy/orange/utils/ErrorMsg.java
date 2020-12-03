91be730d9fece08d0a4ff3b9ad36ceb9af5f8e57
wangwy8 <wangwy8@asiainfo.com>
2020-12-03 18:07:05 +0800

    新增ERROR

---
+++ orange_api/src/main/java/cn/cocowwy/orange/utils/ErrorMsg.java
@@ -0,0 +1,20 @@
+package cn.cocowwy.orange.utils;
+
+import cn.hutool.core.lang.Assert;
+
+/**
+ *@author Cocowwy
+ *@create 2020-12-12-17:50
+ */
+public class ErrorMsg {
+    // 非空Error信息
+    public static String ERROR_BLANK_USERNAME = "用户名不能为空";
+    public static String ERROR_BLANK_PASSWORD = "密码不能为空";
+    public static String ERROR_BLANK_NAME = "用户对外展示昵称不能为空";
+    public static String ERROR_BLANK_ADDRESS1 = "验收地址不能为空不能为空";
+    public static String ERROR_BLANK_SEX = "用户性别不能为空";
+    public static String ERROR_BLANK_PHONE = "手机号码不能为空";
+    public static String ERROR_BLANK_SCHOOLNUMBER = "学院名称不能为空";
+    public static String ERROR_BLANK_USERREALNAME = "用户真实姓名不能为空";
+    public static String ERROR_BLANK_WXID = "可联系微信号不能为空";
+}
